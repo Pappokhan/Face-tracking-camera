848c10478a7322062a90e9839f79e791b5f3dddf
Pappo Khan <110984853+Pappokhan@users.noreply.github.com>
2023-04-06 23:53:51 +0600

    Add files via upload

---
+++ Face tracking camera.py
@@ -0,0 +1,20 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_HSV2BGR)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+    
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    cv2.imshow('Face Tracking Camera', frame)
+    
+    if cv2.waitKey(1) == ord('q'):
+        break
+    
+cap.release()
+cv2.destroyAllWindows()
